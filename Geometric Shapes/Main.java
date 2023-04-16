79d843e136c183d0c60460c360e7dff1cd99eed5
Yousif Hazim <105172397+YousifHazim@users.noreply.github.com>
2023-04-17 00:47:44 +0200

    Create Main.java

---
+++ Geometric Shapes/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Question one :");
+
+        System.out.print("A) The Area of the Rectangle is : ");
+        System.out.println(Area.Rectangle(4,5.5));
+
+        System.out.print("The Perimeter of the Rectangle is : ");
+        System.out.println(Perimeter.Rectangle(4,5.5));
+
+        System.out.print("B) The Area of the Square is : ");
+        System.out.println(Area.Square(10));
+
+        System.out.print("The Perimeter of the Square is : ");
+        System.out.println(Perimeter.Square(10));
+
+        System.out.print("C) The Area of the Circle is : ");
+        System.out.println(Area.Circle(3.2));
+
+        System.out.print("The Perimeter of the Circle is : ");
+        System.out.println(Perimeter.Circle(3.2));
+
+    }
+}
