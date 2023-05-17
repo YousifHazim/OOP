a56f3bac6ed4d4a4be5b1a0085c19d0d3d50ac64
Yousif Hazim <105172397+YousifHazim@users.noreply.github.com>
2023-05-18 02:07:42 +0300

    Update Main.java

--- Vehicle/Main.java
+++ Vehicle/Main.java
@@ -5,25 +5,25 @@ public class Main {
     public static void main(String[] args) {
 
 
-    Vehicle[] vehicles = new Vehicle[2];
+        Vehicle[] vehicles = new Vehicle[2];
 
-    vehicles[0] = new Car(100,4,10);
-    vehicles[1] = new Bike();
-
-
-    try {
-        for (int i=0;i<3;i++){
-            vehicles[0].drive();
-        }
-
-        for (int i=0;i<1;i++){
-            vehicles[1].drive();
-        }
+        vehicles[0] = new Car(100,4,10);
+        vehicles[1] = new Bike();
 
 
-    }catch (NoFuelException ex){
-        System.out.println(ex.getMessage());
-    }
+        try {
+            for (int i=0;i<3;i++){
+                vehicles[0].drive();
+            }
+
+            for (int i=0;i<1;i++){
+                vehicles[1].drive();
+            }
+
+
+        }catch (NoFuelException ex){}
+
+
 
     }
 }
