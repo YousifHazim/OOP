f0f6600f8941e8ce0740dbc9e20d1a785f553529
Yousif Hazim <105172397+YousifHazim@users.noreply.github.com>
2023-05-18 01:47:32 +0300

    Update Vehicle.java

--- Vehicle/Vehicle.java
+++ Vehicle/Vehicle.java
@@ -8,6 +8,10 @@ public abstract class Vehicle {
         this.numberOfWheels = numberOfWheels;
     }
 
+    public int getNumberOfWheels() {
+        return numberOfWheels;
+    }
+
     public abstract void drive();
 
 }
